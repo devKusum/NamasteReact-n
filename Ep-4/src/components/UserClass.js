dccd617a2c5b8259ee8881ae726bfb7cc9c86455
devKusum <163654291+devKusum@users.noreply.github.com>
2024-08-04 20:59:28 +0530

    done

--- Ep-4/src/components/UserClass.js
+++ Ep-4/src/components/UserClass.js
@@ -5,22 +5,23 @@ class UserClass extends React.Component{     //React.Componet given by react
 
    constructor(props){
     super(props);
+
+     this.state = {
+        Name :"useState in classs component",
+        Name2 :"useState in classs component",
+     }
+
    }
-  
-
-
     render() {
     
 
         //destructuring technique---------
         const {name, location} = this.props; 
-
-
-
-
+        const {Name, Name2} = this.state; 
         return(
             <div className="userCard">
-             
+             <h5>name:{Name}</h5>
+             <h5>name:{Name2}</h5>
                <h3>Name:{name}</h3>
                <span>Locatio:{location}</span>
                <h3>Post:Web Developer</h3>
