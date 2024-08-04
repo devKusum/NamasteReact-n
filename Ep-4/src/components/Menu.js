37535e9ad4b813eebf5361df6ad128cec5a7f07c
devKusum <163654291+devKusum@users.noreply.github.com>
2024-08-04 14:57:17 +0530

    done

--- Ep-4/src/components/Menu.js
+++ Ep-4/src/components/Menu.js
@@ -1,42 +1,4 @@
-// import React, { useEffect, useState } from 'react'
-// import { Link, useParams } from 'react-router-dom'
 
-// const Menu = () => {
-//     const { id } = useParams();
-//     const [mealData, setMealData] = useState([])
-//     const { strMealThumb, strArea, strCategory, strMeal, strInstructions, strTags, strYoutube } = mealData;
-
-//     useEffect(() => {
-//         fetchApiData();
-//     }, [id]);
-
-//     const fetchApiData = async () => {
-//         const res = await fetch(`https://api.freeapi.app/api/v1/public/meals/${id}`);
-//         const resData = await res.json();
-//         setMealData(resData?.data);
-//     }
-
-//     return (
-
-//         <div className="meal-card">
-//             <div className="meal-details">
-
-//                 <h2> {strMeal} </h2>
-//                 <p> <span> Category : </span> {strCategory} </p>
-//                 <p>  <span> Tags : {strTags} </span> </p>
-//                 <p> <span> Location : </span> {strArea} </p>
-//                 <p> <span> Instructions : </span> {strInstructions} </p>
-
-//             </div>
-//             <div className="meal-thumb">
-//                 <Link to={strYoutube}> <img src={strMealThumb} alt="card-img" title='Goto Youtube' /> </Link>
-//             </div>
-
-//         </div>
-//     )
-// }
-
-// export default Menu;
 
  import  { useEffect, useState } from 'react'
  import { Link, useParams } from 'react-router-dom'
