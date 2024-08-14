cb6799aa326200beabdd455d7d5a7daca61c4e94
devKusum <163654291+devKusum@users.noreply.github.com>
2024-08-14 08:24:19 +0530

    done

--- Ep-4/src/utils/useOnlineStatus.js
+++ Ep-4/src/utils/useOnlineStatus.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 const useOnlineStatus = () => {
-<<<<<<< HEAD
+
     const [onlineStatus, setOnlineStatus] = useState(true);
 
  
@@ -23,21 +23,7 @@ const useOnlineStatus = () => {
 };
 
 export default useOnlineStatus;
-=======
-  const [onlineStatus, setOnlineStatus] = useState(true);
-  useEffect(() => {
-    window.addEventListener("offline", () => {
-      setOnlineStatus(false);
-    });
 
-    window.addEventListener("online", () => {
-      setOnlineStatus(true);
-    });
-  }, []);
 
-  //boolean value
-  return onlineStatus;
-};
 
-export default useOnlineStatus;
->>>>>>> 1bc6a3c6366fb44db2062b7a246398ff309905a3
+
